b66eaa0aca40f4dc7847e8e1d4751d5ee04652b7
Tkach Leonid <leonid.tkach@nure.ua>
2026-02-05 04:11:45 +0000

    fix: clean requirements.txt and set robust config defaults

--- Lab4/pzpi-23-5-tkach-leonid-lab4/iot-client/config.py
+++ Lab4/pzpi-23-5-tkach-leonid-lab4/iot-client/config.py
@@ -4,6 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SERVER_URL = os.getenv("SERVER_URL", "http://backend:8000")
+    SERVER_URL = os.getenv("SERVER_URL", "http://localhost:8000")
     DEVICE_ID = int(os.getenv("DEVICE_ID", "1"))
     SEND_INTERVAL = int(os.getenv("SEND_INTERVAL", "5"))
